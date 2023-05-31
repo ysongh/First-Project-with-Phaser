fb6177fd92f6e86dc8d19fc13256552f50b34f72
You Song Hou <ysongweb3@gmail.com>
2023-05-30 23:29:22 -0400

    feat: integrate RainbowKit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,42 @@
+import '@rainbow-me/rainbowkit/styles.css';
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import { configureChains, createConfig, WagmiConfig } from 'wagmi';
+import { polygonMumbai } from 'wagmi/chains';
+import { publicProvider } from 'wagmi/providers/public';
+import { ConnectButton } from '@rainbow-me/rainbowkit';
+
 import PhaserGame from './pages/Game';
+
+const { chains, publicClient } = configureChains(
+  [polygonMumbai],
+  [
+    publicProvider()
+  ]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: 'My RainbowKit App',
+  projectId: 'YOUR_PROJECT_ID',
+  chains
+});
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient
+})
 
 function App() {
   return (
-    <>
-      <PhaserGame />
-    </>
+    <WagmiConfig config={wagmiConfig}>
+      <RainbowKitProvider chains={chains}>
+        <ConnectButton />
+        <PhaserGame />
+      </RainbowKitProvider>
+    </WagmiConfig>
   )
 }
 
