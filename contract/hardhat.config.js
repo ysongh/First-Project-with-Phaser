a72aa949ae8a8717b11e3227d2aa99af4e685bb0
You Song Hou <ysongweb3@gmail.com>
2023-06-02 00:49:07 -0400

    feat: set the path in 'hardhat.config.js' to compile the contracts

--- contract/hardhat.config.js
+++ contract/hardhat.config.js
@@ -3,4 +3,9 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
+  // set the path to compile the contracts
+  paths: {
+    artifacts: '../src/artifacts',
+    cache: '../src/cache',
+  }
 };
